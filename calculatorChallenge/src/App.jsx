9e3670666a30f5e00ca17034aa587badf5917c2c
lyssandraa <145362887+lyssandraa@users.noreply.github.com>
2024-06-19 15:13:31 +0100

    installed mathjs and added new component

--- calculatorChallenge/src/App.jsx
+++ calculatorChallenge/src/App.jsx
@@ -1,10 +1,53 @@
 import { useState } from "react";
+import { evaluate } from "mathjs";
 import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+const App = () => {
+  const [sum, setSum] = useState("");
+  const buttons = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0",
+    "-",
+    "+",
+    "*",
+    "/",
+    "=",
+  ];
 
-  return <></>;
-}
+  const handleClick = (btn) => {
+    // If user enters =
+    // Pass the sum into the evaluate function
+    // The Evaluate answer becomes sum after setSum is given the above
+    console.log(btn);
+  };
+
+  return (
+    <div className="centre">
+      <h2>Lysandra's Caluclator App</h2>
+      <h4 className="inputBox">{sum}</h4>
+      <div className="buttonWrap">
+        {buttons.map((button, index) => {
+          return (
+            <button
+              key={index}
+              onClick={() => handleClick(button)}
+              className="btn"
+            >
+              {button}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default App;
